961ae4b611f1c9a04413c712190c5cb30ca20373
AvailableUsernameA <huc014@ucsd.edu>
2025-04-17 15:38:54 -0700

    change append

--- global.js
+++ global.js
@@ -24,10 +24,10 @@ for (let p of pages) {
     let title = p.title;
     url = !url.startsWith('http') ? BASE_PATH + url : url;
     // next step: create link and add it to nav
-    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-    // let a = document.createElement('a');
-    // a.href = url;
-    // a.textContent = title;
-    // nav.append(a);
+    // nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
    }
 
