ff6dd441d8a2e639a6ecefba8f83aa36a8ad1c49
AvailableUsernameA <huc014@ucsd.edu>
2025-04-17 15:11:09 -0700

    nav auto

--- global.js
+++ global.js
@@ -4,11 +4,20 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'CV.html', title: 'CV'},
+    { url: 'https://github.com/AvailableUsernameA', title: 'GitHub'}
+  ];
 
-if (currentLink) {
-    // or if (currentLink !== undefined)
-    currentLink.classList.add('current');
-}
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // next step: create link and add it to nav
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
