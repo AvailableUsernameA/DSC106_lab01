edc2b1267bedafb6b570c8cb26198e2b46e777b4
AvailableUsernameA <huc014@ucsd.edu>
2025-04-25 18:34:05 -0700

    04

--- global.js
+++ global.js
@@ -76,3 +76,59 @@ emailForm?.addEventListener('submit', function (event) {
   console.log(emailurl);
   location.href = emailurl;
 })
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    console.log(response)
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+// export function renderProjects(project, containerElement) {
+// export function renderProjects(projects, containerElement) {
+//   // Your code will go here
+//   containerElement.innerHTML = '';
+//   for (const project of projects) {
+//     const article = document.createElement('article');
+//     article.innerHTML = `
+//     <h3>${project.title}</h3>
+//     <img src="${project.image}" alt="${project.title}">
+//     <p>${project.description}</p>
+// `;
+//     containerElement.appendChild(article);
+//   }
+// }
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  // write javascript that will allow dynamic heading levels based on previous function
+  const titleElement = document.querySelector('.projects-title');
+  if (titleElement!=null) {
+    titleElement.textContent = `${projects.length} Projects`;
+  }
+
+  containerElement.innerHTML = '';
+  for (const project of projects) {
+    const article = document.createElement('article');
+
+    const heading = document.createElement(headingLevel);
+    article.innerHTML = `
+    <${headingLevel}>${project.title}</${headingLevel}>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+`;
+    containerElement.appendChild(article);
+  }
+}
+
+export async function fetchGitHubData(username) {
+  // return statement here
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
